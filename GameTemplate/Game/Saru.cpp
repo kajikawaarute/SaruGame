a4d3c9e3c1143dff71010e666433db2d343ed430
Kajikawa Arute <kbc18b07@stu.kawahara.ac.jp>
2019-11-28 02:56:16 +0900

    サルのアニメーションを追加

--- GameTemplate/Game/Saru.cpp
+++ GameTemplate/Game/Saru.cpp
@@ -5,6 +5,10 @@
 Saru::Saru()
 {
 	m_model.Init(L"Assets/modelData/Saru.cmo");
+	m_animClip[enAnim_taiki].Load(L"Assets/animData/Saru-run.tka");
+	m_animClip[enAnim_taiki].SetLoopFlag(true);
+
+	m_animation.Init(m_model, m_animClip, enAnim_num);
 	m_position.z = 500.0f;
 }
 
@@ -14,6 +18,9 @@ Saru::~Saru()
 
 void Saru::Update()
 {
+	m_animation.Update(1.0f / 30.0f);
+	m_animation.Play(enAnim_taiki);
+
 	//ワールド行列の更新。
 	m_model.UpdateWorldMatrix(m_position, m_rotation, m_scale);
 }
--- GameTemplate/Game/Saru.h
+++ GameTemplate/Game/Saru.h
@@ -14,6 +14,11 @@ private:
 	CQuaternion m_rotation = CQuaternion::Identity();	//回転
 	CVector3 m_scale = CVector3::One();					//拡大率
 	CVector3 m_moveSpeed = CVector3::Zero();			//移動速度
-
+	Animation m_animation;								//アニメーション
+	AnimationClip m_animClip[1];						//アニメーションクリップ
+	enum EnAnimationClip {
+		enAnim_taiki,			//待機アニメーション
+		enAnim_num				//アニメーションクリップの数
+	};
 };
 
