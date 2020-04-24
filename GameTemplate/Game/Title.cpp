d37725e3e7e1578f73382561f189ce4f59be7457
Kajikawa Arute <kbc18b07@stu.kawahara.ac.jp>
2020-04-24 10:15:09 +0900

    タイトル画面を変更

--- GameTemplate/Game/Title.cpp
+++ GameTemplate/Game/Title.cpp
@@ -6,14 +6,37 @@
 Title::Title()
 {
 	m_spriteBatch = g_graphicsEngine->GetSpriteBatch();
-	m_device = g_graphicsEngine->GetDevice();
-	//画像の読み込み
+
+	for (int i = 0; i < 3; i++)
+	{
+		m_device[i] = g_graphicsEngine->GetDevice();
+	}
+
+	//タイトル画像の読み込み
 	DirectX::CreateDDSTextureFromFile(
-		m_device,							//デバイス
-		L"Assets/sprite/title-kari.dds",
+		m_device[0],							//デバイス
+		L"Assets/sprite/Title.dds",				//読み込む画像ファイル
 		nullptr,
-		&m_shaderResourceView				//読み込んだファイルの情報を格納
+		&m_shaderResourceView[0]				//読み込んだファイルの情報を格納
 	);
+
+	//タイトル名の画像の読み込み
+	DirectX::CreateDDSTextureFromFile(
+		m_device[1],							//デバイス
+		L"Assets/sprite/Catch_the_monkey.dds",	//読み込む画像ファイル
+		nullptr,
+		&m_shaderResourceView[1]				//読み込んだファイルの情報を格納
+	);
+	m_positionX[1] = -1500.0f;
+
+	//Press_Start_Buttonの画像の読み込み
+	DirectX::CreateDDSTextureFromFile(
+		m_device[2],							//デバイス
+		L"Assets/sprite/Press_Start.dds",		//読み込む画像ファイル
+		nullptr,
+		&m_shaderResourceView[2]				//読み込んだファイルの情報を格納
+	);
+	m_positionY[2] = 300.0f;
 }
 
 
@@ -23,19 +46,33 @@ Title::~Title()
 
 void Title::Update()
 {
-	if (g_pad[0].IsTrigger(enButtonStart))
+	if (g_pad[0].IsTrigger(enButtonStart) && m_positionX[1] == 0.0f && m_positionY[2] == 0.0f)
 	{
 		m_game = g_goMgr.NewGO<Game>();
 		g_goMgr.DeleteGO(this);
+	}
+	if (m_positionX[1] < 0.0f) {
+		m_positionX[1] += 50.0f;
+	}
+	if (m_positionY[2] > 0.0f) {
+		m_positionY[2] -= 10.0f;
 	}
 }
 
 void Title::Draw()
 {
-	m_spriteBatch->Begin();
-	m_spriteBatch->Draw(
-		m_shaderResourceView,			//読み込んだ画像ファイル
-		DirectX::XMFLOAT2(300.0f, 100.0f)	//画像の座標
-	);
-	m_spriteBatch->End();
+	SpriteBatchDraw();
+}
+
+void Title::SpriteBatchDraw()
+{
+	for (int i = 0; i < 3; i++)
+	{
+		m_spriteBatch->Begin();
+		m_spriteBatch->Draw(
+			m_shaderResourceView[i],							//読み込んだ画像ファイル
+			DirectX::XMFLOAT2(m_positionX[i], m_positionY[i])	//画像の座標
+		);
+		m_spriteBatch->End();
+	}
 }
--- GameTemplate/Game/Title.h
+++ GameTemplate/Game/Title.h
@@ -16,12 +16,21 @@ public:
 	/// タイトルの描画関数
 	/// </summary>
 	void Draw();
-private:
-	ID3D11Device* m_device;									//構築済み
-	ID3D11DeviceContext* m_deviceContext;					//構築済み
-	DirectX::SpriteBatch* m_spriteBatch;					//スプライトバッチ
-	ID3D11ShaderResourceView* m_shaderResourceView;			//読み込んだ画像ファイルの保存先
 
-	Game* m_game = nullptr;
+	/// <summary>
+	/// spriteBatchの描画関数
+	/// </summary>
+	void SpriteBatchDraw();
+private:
+	DirectX::SpriteBatch* m_spriteBatch;					//スプライトバッチ
+
+	ID3D11Device* m_device[3];									//構築済み
+	ID3D11DeviceContext* m_deviceContext[3];					//構築済み
+	ID3D11ShaderResourceView* m_shaderResourceView[3];			//読み込んだ画像ファイルの保存先
+
+	float m_positionX[3] = {0.0f, 0.0f, 0.0f};			//画像のX座標
+	float m_positionY[3] = {0.0f, 0.0f, 0.0f};			//画像のY座標
+
+	Game* m_game = nullptr;				//ゲームのインスタンス
 };
 
