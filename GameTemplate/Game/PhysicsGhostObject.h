36f3266d60cb639ea83b9160921de320f3765547
Kajikawa Arute <kbc18b07@stu.kawahara.ac.jp>
2020-09-03 11:17:45 +0900

    各フォルダに移動。

---
+++ GameTemplate/Game/PhysicsGhostObject.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "BoxCollider.h"
+
+class PhysicsGhostObject
+{
+public:
+	PhysicsGhostObject();
+	~PhysicsGhostObject();
+
+	void CreateBox(CVector3 pos, CQuaternion rot, CVector3 size);
+
+	void Release();
+
+	/// <summary>
+	/// ゴースト作成処理の共通処理
+	/// </summary>
+	/// <param name="pos">座標</param>
+	/// <param name="rot">回転</param>
+	void CreateCommon(CVector3 pos, CQuaternion rot);
+
+	/// <summary>
+	/// 引数で渡されたゴーストオブジェクトが自分自身か判定
+	/// </summary>
+	/// <param name="ghost">ゴーストオブジェクト</param>
+	/// <returns></returns>
+	bool IsSelf(const btCollisionObject& ghost) const
+	{
+		return &ghost == &m_ghostObject;
+	}
+	/// <summary>
+	/// 座標を設定
+	/// </summary>
+	/// <param name="pos"></param>
+	void SetPosition(const CVector3& pos);
+private:
+
+	bool m_isRegistPhysicsWorld = false;	//物理ワールドに登録しているかのどうかのフラグ
+	btGhostObject m_ghostObject;			//ゴースト
+
+	std::unique_ptr<ICollider> m_collider;	//コライダー
+};
+
