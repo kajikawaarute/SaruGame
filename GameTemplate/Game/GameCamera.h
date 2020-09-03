9ca3a604d644f215b66214ac7c2a64997fcaaa96
Kajikawa Arute <kbc18b07@stu.kawahara.ac.jp>
2020-09-04 01:54:58 +0900

    コメントを追加。

--- GameTemplate/Game/GameCamera.h
+++ GameTemplate/Game/GameCamera.h
@@ -8,12 +8,17 @@ public:
 	~GameCamera();
 	void Update();
 	void Draw();
+
+	/// <summary>
+	/// プレイヤーのインスタンスを設定。
+	/// </summary>
+	/// <param name="player">プレイヤーのインスタンス</param>
 	void SetPlayer(Player* player)
 	{
 		pl = player;
 	}
 private:
 	Player* pl = nullptr;		//プレイヤーのインスタンス
-	CVector3 m_toCameraPos;
+	CVector3 m_toCameraPos;		//カメラの座標
 };
 
