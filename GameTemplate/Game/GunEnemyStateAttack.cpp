8a2b60c97aff9fa79569bd43709dd0735c47fef0
Kajikawa Arute <kbc18b07@stu.kawahara.ac.jp>
2020-09-09 11:20:24 +0900

    ガンエネミーがプレイヤーを吹き飛ばすようにした

--- GameTemplate/Game/GunEnemyStateAttack.cpp
+++ GameTemplate/Game/GunEnemyStateAttack.cpp
@@ -5,6 +5,7 @@
 #include "IGameObjectManager.h"
 
 const float GUNENEMY_WAIT_DISTANCE = 700.0f;	//待機状態になる距離。
+const float GUNENEMY_FUTTOBI_DISTANCE = 120.0f;	//プレイヤーを吹き飛ばす距離
 const float GUNENEMY_FUTTOBI_POWER = 2500.0f;	//ガンエネミーのプレイヤーを吹っ飛ばす力。
 
 
@@ -31,6 +32,13 @@ void GunEnemyStateAttack::Update()
 	m_gunEnemy->m_rotation.Multiply(gunEnemyFoward);
 
 	m_gunEnemy->Attack();
+
+	//プレイヤーが近づいたら吹き飛ばす。
+	if (toGunEnemyLen < GUNENEMY_FUTTOBI_DISTANCE) {
+		m_gunEnemy->m_player->SetAttackedPower(toGunEnemyDir * GUNENEMY_FUTTOBI_POWER);
+		m_gunEnemy->m_player->Attacked();
+	}
+
 
 	//待機状態になる範囲。
 	if (toGunEnemyLen > GUNENEMY_WAIT_DISTANCE) {
